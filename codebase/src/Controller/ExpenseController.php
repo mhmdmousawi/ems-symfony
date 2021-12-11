628e5308fdf202452221890bc90c7f60ab2f3681
mhmd mousawi <mohammad.mousawi>
2021-12-11 16:20:48 +0200

    EMS Add dummy APIs for all crud operations on a expenses

--- codebase/src/Controller/ExpenseController.php
+++ codebase/src/Controller/ExpenseController.php
@@ -3,21 +3,66 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route ("/expenses")
- */
+#[Route('/expenses')]
 class ExpenseController extends AbstractController
 {
-    /**
-     * @Route ("/{id}")
-     *
-     * @return JsonResponse
-     */
-    public function test($id)
+    #[Route(path: '', methods: ['GET'])]
+    public function getAllExpensesAction(): Response
     {
-        return $this->json(['id' => $id]);
+        //@TODO Add actual logic
+
+        return $this->json([
+            'expenses' => [
+                ['id' => 1],
+                ['id' => 2]
+            ]
+        ], Response::HTTP_OK);
+    }
+
+    #[Route('/{id}', methods: ['GET'])]
+    public function getExpenseAction(int $id): Response
+    {
+        //@TODO Add actual logic
+
+        return $this->json(['id' => $id], Response::HTTP_OK);
+    }
+
+    #[Route(
+        path: '',
+        methods: ['POST']
+    )]
+    public function createExpenseAction(Request $request): Response
+    {
+        //@TODO Add actual logic
+        $data = json_decode($request->getContent(), true);
+
+        return $this->json($data, Response::HTTP_CREATED);
+    }
+
+    #[Route(
+        path: '/{id}',
+        methods: ['PUT']
+    )]
+    public function updateExpenseAction(Request $request, int $id): Response
+    {
+        //@TODO Add actual logic
+        $data = json_decode($request->getContent(), true);
+
+        return $this->json([$data, $id], Response::HTTP_OK);
+    }
+
+    #[Route(
+        path: '/{id}',
+        methods: ['DELETE']
+    )]
+    public function deleteExpenseAction(int $id): Response
+    {
+        //@TODO Add actual logic
+
+        return $this->json(null, Response::HTTP_NO_CONTENT);
     }
 }
